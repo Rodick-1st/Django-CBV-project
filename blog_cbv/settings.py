3a40ff9fd0fac2e68008a138cc629160e1a9b8e4
newco_rodick <newcompik1999@mail.ru>
2025-10-04 09:37:17 +0300

    Создание древовидной модели категорий + админка

--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -43,7 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.blog.apps.BlogConfig',
+
+
+    'apps.blog.apps.BlogConfig',        #ПРИЛОЖЕНИЕ BLOG
+    'mptt',                             #алгоритм для работы с древовидными (иерархическими) данными
+    'django_mptt_admin',                #Дополнение, админа к алгосу MPTT который выше
+
 ]
 
 MIDDLEWARE = [
@@ -113,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -125,7 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = (BASE_DIR / 'static')
+
+MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
