10603c398f20e407c9d28f116a70e24d0bf84994
newco_rodick <newcompik1999@mail.ru>
2025-10-13 20:23:45 +0300

    Добавил кастомные обработчики исключений, но они работают тольки при debug=True а также ALLOWED_HOSTS = ['127.0.0.1']. Это последняя правка в первом курсе по ДЖАНГО, УРА!

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.views.generic import ListView, DetailView, UpdateView
 from django.views.generic import CreateView
 from django.http import JsonResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.utils.formats import date_format
 from django.utils.timezone import localtime
 from taggit.models import Tag
@@ -185,3 +185,32 @@ class RatingCreateView(View):
                 rating.save()
         return JsonResponse({'rating_sum': rating.post.get_sum_rating()})
 
+
+def tr_handler404(request, exception):
+    """
+    Обработка ошибки 404
+    """
+    return render(request=request, template_name='errors/error_page.html', status=404, context={
+        'title': 'Страница не найдена: 404',
+        'error_message': 'К сожалению такая страница была не найдена, или перемещена',
+    })
+
+
+def tr_handler500(request):
+    """
+    Обработка ошибки 500
+    """
+    return render(request=request, template_name='errors/error_page.html', status=500, context={
+        'title': 'Ошибка сервера: 500',
+        'error_message': 'Внутренняя ошибка сайта, вернитесь на главную страницу, отчёт об ошибке мы направим администрации сайта',
+    })
+
+
+def tr_handler403(request, exception):
+    """
+    Обработка ошибки 403
+    """
+    return render(request=request, template_name='errors/error_page.html', status=403, context={
+        'title': 'Ошибка доступа: 403',
+        'error_message': 'Доступ к этой странице ограничен',
+    })
